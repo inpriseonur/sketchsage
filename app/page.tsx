0fcc41a052bfd25de4a6017af16c45e3cb67b85d
Onur Cinemre <onurcinemre@gmail.com>
2025-11-08 00:50:55 +0300

    Fix: Add padding-top to main (pt-10 md:pt-16), increase container to 1280px

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
     <div className="relative flex min-h-screen w-full flex-col bg-[#221010]">
       <div className="flex h-full grow flex-col">
         <div className="px-4 sm:px-8 md:px-12 lg:px-20 flex flex-1 justify-center py-5">
-          <div className="flex flex-col max-w-[1200px] flex-1">
+          <div className="flex flex-col max-w-[1280px] flex-1">
             <Header />
-            <main className="flex flex-col gap-12 md:gap-16 lg:gap-20">
+            <main className="flex flex-col gap-12 md:gap-16 lg:gap-20 pt-10 md:pt-16">
               <Hero />
               <QualitySection />
               <HowItWorks />
